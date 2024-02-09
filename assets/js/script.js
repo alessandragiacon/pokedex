3833804647e9100a68fe0f2b973e7146bbb10dc1
alessandragiacon <alessandragiacon@gmail.com>
2024-02-08 23:05:08 -0300

    adjusts in CSS layout and include a year with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
 async function apiConection(){
-    let currentDate = new Date();
-    let currentYear = currentDate.getFullYear();
-
-    await fetch('https://pokeapi.co/api/v2/pokedex/').then((Response) => Response.json())
+    await fetch('https://pokeapi.co/api/v2/pokemon/').then((Response) => Response.json())
     .then((data) => {
         console.log(data.results);
     });
-    console.log(currentYear);
 }
 
-window.addEventListener('load', apiConection());
+// Função para injetar o ano no footer
+function injectYear(){
+    let currentDate = new Date();
+    let currentYear = currentDate.getFullYear();
+    let year = document.getElementById("copyrightYear");
+    year.innerText = currentYear;
+}
+
+window.addEventListener('load', injectYear());
